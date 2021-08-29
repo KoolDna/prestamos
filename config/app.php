4cadadaa1edbae77a9c934c57b3e1bc61e37225a
mad <mario.a.delgadillo@nube.unadmexico.mx>
2021-08-29 15:31:42 -0500

    Sistema Prestamos 29082021

---
+++ config/app.php
@@ -0,0 +1,5 @@
+<?php
+    const SERVERURL="http://localhost/prestamos/sistema/";
+    const COMPANY = "SISTEMA PRESTAMO";
+    const MONEDA = "$";
+    date_default_timezone_set("America/Mexico_City");
---
+++ config/server.php
@@ -0,0 +1,12 @@
+<?php
+    const SERVER = "localhost";
+    const DB = "prestamos";
+    const USER = "mad";
+    const PASS = "Avatar2910";
+    #Sistema Gestor de Base de Datos
+    const SGBD = "mysql:host=".SERVER.";dbname=".DB;
+    #Contraseñas y otros parametros
+    const METOD = "AES-256-CBC";
+    const SECRET_KEY = '$PRESTAMOS@2020';
+    const SECRET_ID = '037970';
+    
